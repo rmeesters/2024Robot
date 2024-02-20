31216d89562ff154d26f17ca6267589cd260028d
Travise <travise42@icloud.com>
2024-02-20 01:01:01 -0500

    Added control over climber

---
+++ src/main/java/frc/robot/commands/LiftCommand.java
@@ -0,0 +1,47 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.PneumaticsHandler;
+
+public class LiftCommand extends Command {
+
+    private final Climber s_Climber = RobotContainer.s_Climber;
+
+    private final PneumaticsHandler h_pneumatics = RobotContainer.h_pneumatics;
+
+    private final double speed;
+
+    private final Timer timer = new Timer();
+
+    public LiftCommand(double speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public void initialize() {
+        h_pneumatics.releaseClimber();
+    }
+
+    @Override
+    public void execute() {
+        if (timer.hasElapsed(0.2))
+            s_Climber.setSpeed(speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        s_Climber.setSpeed(0);
+
+        new SequentialCommandGroup(
+            new WaitCommand(0.2),
+            new InstantCommand(() -> h_pneumatics.lockClimber())
+        ).schedule();
+    }
+    
+}
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,11 +7,8 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
 
 public class Climber extends SubsystemBase {
-
-    private PneumaticsHandler h_pneumatics = RobotContainer.h_pneumatics;
 
     private TalonFX fxClimberMotor;
     private TalonFXConfiguration fxConfig;
@@ -40,7 +37,6 @@ public class Climber extends SubsystemBase {
             speedPercent = 0;
         }
 
-        h_pneumatics.lockClimber(speedPercent == 0);
         fxClimberMotor.set(speedPercent);
     }
 
